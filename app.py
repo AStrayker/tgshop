db740bc6edeb766b7623d48aecb579963bef4531
AStrayker <astrajker@gmail.com>
2025-01-28 10:01:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ products = [
     {
         "id": 1,
         "title": "Куртка зимняя",
-        "image": "https://ager.ua/image/cachewebp/catalog/import_files/1e/72992fb7-e030-11e0-80a9-0015c56feef5/1eb1b54d-c1d4-11ee-8588-d8cb8a9c13c3-414x620.webp",
+        "image": "https://borec.in.ua/5463-medium_default/kurtka-manto-winter-jacket-alpha.jpg",
         "description": "Теплая зимняя куртка с отличной защитой от холода.",
         "details": ["Материал: 100% полиэстер", "Цвет: Черный", "Размеры: S, M, L, XL"]
     },
     {
         "id": 2,
         "title": "Куртка летняя",
-        "image": "https://ager.ua/image/cachewebp/catalog/import_files/40/feee7c9d-e02c-11e0-80a9-0015c56feef5/40552647-bb8b-11ee-8588-d8cb8a9c13c3-6-414x620.webp",
+        "image": "https://borec.in.ua/5463-medium_default/kurtka-manto-winter-jacket-alpha.jpg",
         "description": "Легкая куртка для прохладных летних вечеров.",
         "details": ["Материал: Хлопок", "Цвет: Бежевый", "Размеры: S, M, L"]
     }
